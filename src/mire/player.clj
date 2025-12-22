cb0b27db5b2dcb7ed6dd980629537a800b434ec1
tokito <160491609+tokitostr@users.noreply.github.com>
2025-12-23 02:46:30 +0300

    Update player.clj: add coins, level and experience

--- src/mire/player.clj
+++ src/mire/player.clj
@@ -3,9 +3,20 @@
 (def ^:dynamic *current-room*)
 (def ^:dynamic *inventory*)
 (def ^:dynamic *name*)
+(def ^:dynamic *coins*)
+(def ^:dynamic *level*)
+(def ^:dynamic *experience*)
 
 (def prompt "> ")
 (def streams (ref {}))
 
 (defn carrying? [thing]
   (some #{(keyword thing)} @*inventory*))
+
+(defn get-coins []
+  @*coins*)
+
+
+(defn add-coins [amount]
+  (dosync
+   (alter *coins* + amount)))
